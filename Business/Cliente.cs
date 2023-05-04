d7eb7ddabfdf34dff8acccdae80dc5c387c22ce6
curso.ads2 <curso.ads2@LAB6190>
2023-05-03 22:02:37 -0300

    Adicionar arquivos de projeto.

---
+++ Business/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class Cliente : Base
+    {
+        [OpcoesBase(ChavePrimaria = true, UsaBD =true, UsaBusca = true)]
+        public int ID { get; set; }
+        [OpcoesBase(UsaBD =true)]
+        public string Nome { get; set; }
+        [OpcoesBase(UsaBD =true)]
+        public string Celular { get; set; }
+        [OpcoesBase(UsaBD =true)]
+        public string CPF { get; set; }
+
+    }
+}
